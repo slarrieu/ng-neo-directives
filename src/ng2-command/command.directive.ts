1d4f8c302a3f784c61f4d22ed566c3f44d928a56
Sebastian Larrieu <slarrieu@team.neocomplexx.com>
2018-07-26 13:54:27 -0300

    New version with delay and device detection

--- src/ng2-command/command.directive.ts
+++ src/ng2-command/command.directive.ts
@@ -10,15 +10,13 @@ import {
 	ElementRef,
 	InjectionToken
 } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subscription, Observable, Subject, BehaviorSubject } from 'rxjs';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/observable/combineLatest';
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
 
 export const COMMAND_CONFIG = new InjectionToken<string>('COMMAND_CONFIG');
 
@@ -51,6 +49,7 @@ export class CommandDirective implements OnInit, OnDestroy {
 	@Input() commandOptions: CommandOptions;
 	@Input() commandCanExecute: boolean;
 	@Input() commandValue: any;
+	@Input() commandDelay: number;
 	// @HostBinding('disabled') isDisabled: boolean;
 
 	private canExecute$$: Subscription;
@@ -63,6 +62,20 @@ export class CommandDirective implements OnInit, OnDestroy {
 		private renderer: Renderer,
 		private element: ElementRef
 	) {
+		if (this.isMobileOperatingSystem()) {
+			element.nativeElement.addEventListener('touch', async (event: MouseEvent) => {
+				event.preventDefault();
+				console.log('[commandDirective::onClick]');
+				this.command.execute(this.commandValue);
+			});
+		} else {
+			element.nativeElement.addEventListener('click', async (event: MouseEvent) => {
+				console.log("Command click");
+				event.preventDefault();
+				console.log('[commandDirective::onTouch]');
+				this.command.execute(this.commandValue);
+			});
+		}
 	}
 
 	ngOnInit() {
@@ -89,11 +102,22 @@ export class CommandDirective implements OnInit, OnDestroy {
 			}).subscribe();
 	}
 
-	@HostListener('click')
-	onClick() {
-		// console.log('[commandDirective::onClick]');
-		this.command.execute(this.commandValue);
-	}
+	//@HostListener('click', ['$event', 'true'])
+	//onClick(event: MouseEvent) {
+	//	if (this.isMobileOperatingSystem()) return;
+	//	event.preventDefault();
+	//	debugger;
+	//	// console.log('[commandDirective::onClick]');
+	//	this.command.execute(this.commandValue);
+	//}
+
+	// @HostListener('touch', ['$event', 'true'])
+	// onTouch(event: MouseEvent) {
+	// 	event.preventDefault();
+	// 	debugger;
+	// 	// console.log('[commandDirective::onClick]');
+	// 	this.command.execute(this.commandValue);
+	// }
 
 	ngOnDestroy() {
 		// console.log('[commandDirective::destroy]');
@@ -101,6 +125,26 @@ export class CommandDirective implements OnInit, OnDestroy {
 		if (this.canExecute$$) { this.canExecute$$.unsubscribe(); }
 		if (this.isExecuting$$) { this.isExecuting$$.unsubscribe(); }
 	}
+
+	private isMobileOperatingSystem(): boolean {
+		var userAgent = navigator.userAgent || navigator.vendor;
+	  
+			// Windows Phone must come first because its UA also contains "Android"
+		  if (/windows phone/i.test(userAgent)) {
+			  return true; // "Windows Phone";
+		  }
+	  
+		  if (/android/i.test(userAgent)) {
+			  return true; // "Android";
+		  }
+	  
+		  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+		  if (/iPad|iPhone|iPod/.test(userAgent)) {
+			  return true; // "iOS";
+		  }
+	  
+		  return false; // "unknown";
+	  }
 }
 
 export interface CommandOptions {
@@ -168,11 +212,13 @@ export class Command implements ICommand {
 	 * @param {(() => any)} executeParam Execute function to invoke - use `isAsync: true` when {Observable<any>}.
 	 * @param {Observable<boolean>} [canExecute] Observable which determines whether it can execute or not.
 	 * @param {boolean} [isAsync] Indicates that the execute function is async e.g. Observable.
+	 * @param {number} [delay] Indicates that the execute function is dealy by given milliseconds.
 	 */
 	constructor(
 		private executeParam: (any?) => any,
 		canExecute$?: Observable<boolean>,
-		private isAsync?: boolean
+		private isAsync?: boolean,
+		private delay?: number
 	) {
 		if (canExecute$) {
 			this.canExecute$ = Observable.combineLatest(
@@ -195,12 +241,12 @@ export class Command implements ICommand {
 				.do(x => this.isExecuting = x)
 				.subscribe();
 		}
-		this.buildExecutionPipe(executeParam, isAsync);
+		this.buildExecutionPipe(executeParam, isAsync, delay);
 	}
 
 	public verifyCommandExecutionPipe() {
 		if (this.executionPipe$.observers.length === 0) {
-			this.buildExecutionPipe(this.executeParam, this.isAsync);
+			this.buildExecutionPipe(this.executeParam, this.isAsync, this.delay);
 		}
 	}
 
@@ -228,7 +274,9 @@ export class Command implements ICommand {
 		}
 	}
 
-	private buildExecutionPipe(executeParm: (any?) => any, isAsync?: boolean) {
+	private delaySubscribe: Subscription;
+
+	private buildExecutionPipe(executeParm: (any?) => any, isAsync?: boolean, delay?: number) {
 		let pipe$ = this.executionPipe$
 			.filter(() => this.canExecute)
 			.do(() => {
@@ -237,10 +285,15 @@ export class Command implements ICommand {
 			});
 
 		pipe$ = isAsync
-			? pipe$.switchMap((value) => {
-				this.result = executeParm(value);
-				console.log(this.result);
-				return this.result;
+			? pipe$.switchMap((value) => { 
+				if (delay && delay > 0) {
+					if (this.delaySubscribe) this.delaySubscribe.unsubscribe();
+					const timer = Observable.timer(delay) as TimerObservable;
+					this.delaySubscribe = timer.subscribe(t => executeParm(value));
+					return Promise.resolve(null);
+				} else { 		
+					return executeParm(value); 
+				} 
 			})
 			: pipe$.do((value) => executeParm(value));
 
@@ -252,7 +305,7 @@ export class Command implements ICommand {
 				(e) => {
 					console.log('[command::excutionPipe$] do#2 error - set idle' + e.toString());
 					this.isExecuting$.next(false);
-					this.buildExecutionPipe(executeParm, isAsync);
+					this.buildExecutionPipe(executeParm, isAsync, delay);
 				});
 		this.executionPipe$$ = pipe$.subscribe();
 	}
