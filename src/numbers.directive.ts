32413f89804fc49d10858c9812bb387c46a5969c
Sebastian Larrieu <slarrieu@team.neocomplexx.com>
2018-08-28 12:03:56 -0300

    New version with selection of number in isNumber directive

--- src/numbers.directive.ts
+++ src/numbers.directive.ts
@@ -10,10 +10,16 @@ export class NumbersDirective {
         this.el = this._el;
     }
 
+    @HostListener('focus') onFocus() {
+        if (this.el.nativeElement.valueAsNumber === 0) {
+            this.el.nativeElement.select();
+        }
+    }
+
     @HostListener('keydown', ['$event']) onKeyDown(e: KeyboardEvent) {
         if (e.key === 'Delete' || e.key === 'Tab' || e.key === 'Backspace' || e.key === 'ArrowLeft' ||
             e.altKey || e.ctrlKey || e.key === 'Home' || e.key === 'End' || e.key === 'PageDown' || e.key === 'PageUp' ||
-            e.key === 'ArrowRight' || e.key === 'Control' || e.key === 'Alt'  || e.key === 'Shift') { return; }
+            e.key === 'ArrowRight' || e.key === 'Control' || e.key === 'Alt' || e.key === 'Shift') { return; }
 
         if (!(e.key >= '0' && e.key <= '9')) {
             e.preventDefault();
